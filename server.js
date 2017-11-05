3f9d318c0219be267864d67ad5b5f84183d45982
tjallen27 <tjallen91@hotmail.co.uk>
2017-11-05 16:37:02 +0000

    Comments added to server.js

--- server.js
+++ server.js
@@ -2,16 +2,22 @@ const express = require('express');
 const bodyParser = require('bodyParser');
 var app = express();
 
+// BodyParser allows us to get the data from a Post
 app.use(bodyParser.urlencoded({encoded: true}));
 app.use(bodyParser.json());
 
+// Sets the port address
 var port = process.env.PORT || 8080;
 var router = express.Router();
 
+// Sets the routes for the API
 router.get('/', function(req, res){
   res.json({ message: 'Welcome to the API!'});
 });
 
+// States to use the '/api' prefix for all router calls
 app.use('/api', router);
+
+// Start the server
 app.listen(port);
 console.log('Magic happens on port' + port);
